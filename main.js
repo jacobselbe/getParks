52deb6bcd0b1cf092a112754cf57cf8d66fddbfb
Jacob Selbe <jalxselbe@gmail.com>
2018-12-21 17:32:16 -0600

    event listener functional, get api and display api not functional yet

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-//do some logic
+//watch form
+function watchForm() {
+    console.log('watchForm() ready');
+    $('#js-form').submit(e => {
+        e.preventDefault();
+        const searchState = $('#js-searchState').val();
+        const maxResults = $('#js-maxResults').val();
+        console.log(searchState, maxResults);
+        getParks(searchState, maxResults);
+    });
+}
+
+//make api call
+function getParks(searchState, maxResults) {
+    //stuff
+}
+
+//display response
+function displayResponse(stuff) {
+    //stuff
+}
+
+//set page
+$(setPage => {
+    console.log('page set');
+    watchFrom();
+})
